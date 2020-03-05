dedb9f759dbe55de5ca875ca6965c6a86ca566e5
NORIKIM <jinagold.k@gmail.com>
2020-03-05 09:21:43 +0900

    Create)오토레이아웃 프로젝트

---
+++ Basic/Basic1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic1
+//
+//  Created by 김지나 on 2020/02/11.
+//  Copyright © 2020 김지나. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
